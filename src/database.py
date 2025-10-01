6be76242e5838f7895fe8ea36297d636de3ed156
Danil <Nabolnad@gmail.com>
2025-10-01 22:23:47 +0300

    Добавил миграции к базе данных

--- src/database.py
+++ src/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from src.config import settings
+from sqlalchemy.orm import DeclarativeBase
 
 engine = create_async_engine(settings.DB_URL)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
 
 # Пример создания сессии для работы с бд, в поле execute будет какой-то код на выполнение.
 # session = async_session_maker()
