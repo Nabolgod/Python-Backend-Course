2b0bc884fb806ebfd31baa788dd9d420d15dca77
Danil <Nabolnad@gmail.com>
2025-12-12 05:19:13 +0300

    добавил кэширование через асинхронный redis

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Query
 from src.api.dependencies import DBDep
 from src.schemes.facilities import FacilityAdd
+from fastapi_cache.decorator import cache
 
 router = APIRouter(prefix="/facilities", tags=["Удобства"])
 
 
-@router.post("/", summary="Создать удобство")
+@router.post("", summary="Создать удобство")
 async def create_facility(
         db: DBDep,
         facility_data: FacilityAdd,
@@ -16,15 +17,18 @@ async def create_facility(
     return {"status": "ok", "data": facility_data}
 
 
-@router.get("/", summary="Вернуть все удобства")
+@router.get("", summary="Вернуть все удобства")
+@cache(expire=60)
 async def get_facilities(
         db: DBDep,
         title: str | None = Query(default=None, description="Название удобства"),
 ):
+    print("Иду в базу данных")
     return await db.facilities.get_all_facilities(title=title)
 
 
 @router.get("/{facility_id}", summary="Вернуть удобство по ID")
+@cache(expire=60)
 async def get_facility(
         db: DBDep,
         facility_id: int,
--- src/main.py
+++ src/main.py
@@ -1,18 +1,37 @@
-from fastapi import FastAPI
 import uvicorn
 import sys
+
 from pathlib import Path
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
 
 # Магическая команда для корректного поиска путей
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.connectors.redis_connector import redis_connector
 from src.api.auth import router as auth_router
 from src.api.hotels import router as hotels_router
 from src.api.rooms import router as rooms_router
 from src.api.bookings import router as bookings_router
 from src.api.facilities import router as facilities_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    # При старте приложения
+    await redis_connector.connect()
+    FastAPICache.init(RedisBackend(redis_connector.redis), prefix="fastapi-cache")
+    yield
+    # При выключении/перезагрузке приложения
+    await redis_connector.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(auth_router)
 app.include_router(hotels_router)
 app.include_router(rooms_router)
